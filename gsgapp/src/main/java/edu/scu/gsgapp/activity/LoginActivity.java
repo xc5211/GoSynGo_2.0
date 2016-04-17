3a3b5e22a671400bb0e9d239780f8f22daf3fee8
cxu <xc5211@gmail.com>
2016-04-17 16:30:44 -0700

    Fixed xml path refered to activity.

--- gsgapp/src/main/java/edu/scu/gsgapp/activity/LoginActivity.java
+++ gsgapp/src/main/java/edu/scu/gsgapp/activity/LoginActivity.java
@@ -117,7 +117,7 @@ public class LoginActivity extends GsgBaseActivity {
         });
     }
 
-    public void login(View view) {
+    public void login() {
         String email = this.emailEdit.getText().toString();
         String password = this.passwordEdit.getText().toString();
         this.loginButton.setEnabled(false);
