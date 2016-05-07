df7671e6ae92c45a8e735f113dcbcc794f62508a
Diabloblood <huangsichao2015@gmail.com>
2016-05-07 09:30:28 -0700

    add load and save host person to GsgBaseActivity

--- gsgapp/src/main/java/edu/scu/gsgapp/activity/GsgBaseActivity.java
+++ gsgapp/src/main/java/edu/scu/gsgapp/activity/GsgBaseActivity.java
@@ -4,8 +4,20 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.io.StreamCorruptedException;
+import java.util.HashMap;
+
 import edu.scu.core.AppAction;
 import edu.scu.gsgapp.GsgApplication;
+import edu.scu.model.Person;
 
 /**
  * Created by chuanxu on 4/16/16.
@@ -15,6 +27,8 @@ public abstract class GsgBaseActivity extends AppCompatActivity {
     public Context context;
     public GsgApplication gsgApplication;
     public AppAction appAction;
+    //sichao
+    public static String fileUri = "PersonMap";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +36,46 @@ public abstract class GsgBaseActivity extends AppCompatActivity {
         context = getApplicationContext();
         gsgApplication = (GsgApplication) this.getApplication();
         appAction = gsgApplication.getAppAction();
+    }
+
+    public void saveHostPerson() {
+        ObjectOutput out = null;
+        try {
+            out = new ObjectOutputStream(new FileOutputStream(new File(getFilesDir(),"") + File.separator + fileUri));
+
+            // TODO: check person
+            if(appAction.getHostPerson() != null) {
+                out.writeObject(new HashMap<String, Person>().put(appAction.getHostPerson().getUserId(), appAction.getHostPerson()));
+            }
+            out.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void loadHostPerson(String userID) {
+        ObjectInputStream input = null;
+        try {
+            input = new ObjectInputStream(new FileInputStream(new File(new File(getFilesDir(),"") + File.separator + fileUri)));
+            HashMap<String, Person> buffedPersonMap = (HashMap<String, Person>) input.readObject();
+
+            // TODO: check person
+            if(buffedPersonMap.get(userID) != null) {
+                this.appAction.setHostPerson(buffedPersonMap.get(userID));
+            }
+
+            input.close();
+        } catch (StreamCorruptedException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
 }
