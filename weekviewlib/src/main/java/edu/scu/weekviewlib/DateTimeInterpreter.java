35e86d5727742c6d0d79d31865dc7406ada48126
cxu <xc5211@gmail.com>
2016-05-30 13:21:19 -0700

    Incorporated 3rd party calendar week/day view as a separate module.

---
+++ weekviewlib/src/main/java/edu/scu/weekviewlib/DateTimeInterpreter.java
@@ -0,0 +1,11 @@
+package edu.scu.weekviewlib;
+
+import java.util.Calendar;
+
+/**
+ * Created by Raquib on 1/6/2015.
+ */
+public interface DateTimeInterpreter {
+    String interpretDate(Calendar date);
+    String interpretTime(int hour);
+}
